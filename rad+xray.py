0fba2a53eb93493971149dcc9434de1b8855e46d
TimWhite <36320909+timwhitez@users.noreply.github.com>
2021-07-08 11:14:32 +0800

    Update rad+xray.py

--- rad+xray.py
+++ rad+xray.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 # coding: utf-8
 import subprocess
+import platform
+import os
 
 def main(data1):
 	target = data1
-	cmd = ["./rad.exe","-t",target,"-http-proxy","127.0.0.1:7777"]
-	rsp=subprocess.Popen(cmd)
-	output, error = rsp.communicate()
-	print(output)
+	print(target + "crawling")
+	cmd = ["./rad","-t",target,"-http-proxy","127.0.0.1:7777"]
+	try:
+		output = subprocess.check_output(cmd, timeout=3600)
+		#print(output)
+	except:
+		return
 
 
 if __name__=='__main__':
@@ -15,3 +20,10 @@ if __name__=='__main__':
 	for text in file.readlines():
 		data1=text.strip('\n')
 		main(data1)
+		print(data1 + "finish")
+		time.sleep(30)
+		sysstr = platform.system()
+		if(sysstr =="Windows"):
+			os.system("taskkill /f /IM chrome.exe")
+		elif(sysstr =="Linux"):
+			os.system("ps aux | awk '/chrome/ { print $2 } ' | xargs kill -9")
